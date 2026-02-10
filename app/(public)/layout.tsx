e2f0083a16cb4fb109c86964aa12411706f8a732
Onur Oran <onur.oran@metro.istanbul>
2026-02-10 07:28:15 +0000

    ide zayif

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -36,8 +36,8 @@ export default function PublicLayout({
         {children}
       </main>
 
-      {/* Footer */}
-      {/*<footer className="bg-white border-t border-border/50 py-8 mt-auto">
+      {/* Footer
+      <footer className="bg-white border-t border-border/50 py-8 mt-auto">
         <div className="container mx-auto px-4">
           <div className="flex flex-col md:flex-row items-center justify-between gap-4">
             <div className="flex items-center gap-3">
@@ -56,7 +56,8 @@ export default function PublicLayout({
             </p>
           </div>
         </div>
-      </footer>*\}
+      </footer> */}
+
     </div>
   );
 }
